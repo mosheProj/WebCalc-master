cec1570371bb8a0b921971264038d972292ce8f8
Moshe <mosheozer1977@gmail.com>
2018-09-11 22:31:26 +0300

    eyal shalom

--- WebCalc/Controllers/HomeController.cs
+++ WebCalc/Controllers/HomeController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebCalc.Interfaces;
-using WebCalc.Models;
 
 namespace WebCalc.Controllers
 {
